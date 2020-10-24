1e81960b5db8489c574893eb6626c1494b395eff
mostafa-hammud <mostafa.hammoud199@hotmail.com>
2020-10-24 05:58:36 +0300

    The first version of a blog website

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
+const loDash = require("lodash");
+
+let posts = [];
 
 const homeStartingContent = "sad da ds a d as adkasdkaldama ddadkadsk ad ksad kda dakdkaskdaskdkkfslk kdlsadks ksdfldfssdl  sdkkllk dsklf";
 const aboutContent = "dkasdkad  dkasskssdk  dksadmkm kdskfsm skffskfmd fskfsdkmdf sdfksmf";
@@ -15,9 +18,43 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static(__dirname + "/public"));
 
 app.get("/", (req, res) => {
-    res.render('mainPage');
+    res.render('mainPage', {data: homeStartingContent,posts:posts});    
 });
 
+app.get("/about", (req,res) => {
+    res.render('aboutMe', {data : aboutContent});
+})
+
+app.get("/contact", (req,res) =>{
+    res.render('contactMe', {data: Contact});
+});
+app.get("/write", (req,res) =>{
+    res.render('write');
+})
+
+app.post("/write", (req,res) =>{
+    const post = {
+        title : req.body.newTitle,
+        message : req.body.newMessage
+    };
+    posts.push(post);
+    res.redirect("/")
+})
+
+app.get("/posts/:postName",(req,res) =>{
+    const postTitle = req.params.postName;
+    posts.forEach((i) => {
+        if (loDash.lowerCase(i.title) === loDash.lowerCase(postTitle)){
+            console.log("found");
+            res.render('post', {post: i});
+        }
+    });
+    
+})
+
+app.post("/post", (req,res) => {
+    res.redirect("/posts/:postName");
+})
 
 app.listen(3000, () => {
     console.log("running server on port 3000");
